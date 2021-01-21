6288f9b054f41ea546b7ecd48b3d961bb93b79c8
LALIT ARYAN <lalitaryan1993@gmail.com>
2021-01-21 17:28:18 +0530

    Adding files for github pages

--- src/App.js
+++ src/App.js
@@ -1,7 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import { Component } from "react";
 
-function App() {
+import { CardList } from "./components/card-list/card-list.components";
+
+import { SearchBox } from "./components/search-box/search-box.component";
+// import logo from "./logo.svg";
+import "./App.css";
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      monsters: [],
+      searchField: "",
+    };
+  }
+
+  componentDidMount() {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((response) => response.json())
+      .then((users) => this.setState({ monsters: users }));
+  }
+
+  render() {
+    const { monsters, searchField } = this.state;
+    const filteredMonsters = monsters.filter((monster) =>
+      monster.name.toLowerCase().includes(searchField.toLowerCase())
+    );
+    return (
+      <div className="App">
+        <h1>Monsters Rolodex</h1>
+        <SearchBox
+          placeholder="Search monsters"
+          handleChange={(e) => this.setState({ searchField: e.target.value })}
+        />
+        <CardList monsters={filteredMonsters}></CardList>
+      </div>
+    );
+  }
+}
+
+/* function App() {
   return (
     <div className="App">
       <header className="App-header">
@@ -20,6 +58,6 @@ function App() {
       </header>
     </div>
   );
-}
+} */
 
 export default App;
